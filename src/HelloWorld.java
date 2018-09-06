1b6556344904af919579b8c23f470e89522b4831
Kirill.Vasilyev <Kirill.Vasilyev@sidenis.com>
2018-09-06 13:09:10 +0700

    commit for alter

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World на русском");
         System.out.println("4");
+
+        System.out.println("For alter");
     }
 }
