449d579c492227c0829e5379580306bd5fb78fc1
Kirill.Vasilyev <Kirill.Vasilyev@sidenis.com>
2018-09-06 12:54:22 +0700

    Second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World на русском");
         System.out.println("4090");
-        System.out.println("4431223");
     }
 }
